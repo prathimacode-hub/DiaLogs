86ecdcf16a8aae10311a4b361bd1cd3c9d7df000
Prathima Kadari <kadariprathima4@gmail.com>
2021-08-01 13:24:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,request,render_template
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import pickle
+
+#pip freeze > requirements.txt
+app=Flask(__name__)
+
+@app.route('/kidneydisease',methods=['GET','POST'])
+def kidney():
+	if request.method=='GET':
+		return render_template('kidneyform.html')
+	else:
+		with open('kidney_predict','rb') as f:
+			model=pickle.load(f)
+		Age=int(request.form['Age'])
+		Blood_Pressure=int(request.form['Blood_Pressure'])
+		Specific_Gravity=float(request.form['Specific_Gravity'])
+		Albumin=int(request.form['Albumin'])
+		Sugar=int(request.form['Sugar'])
+		Red_Blood_Cells=int(request.form['Red_Blood_Cells'])
+		new=np.array([[Age,Blood_Pressure,Specific_Gravity,Albumin,Sugar,Red_Blood_Cells]])
+		y_pred=model.predict(new)
+		return render_template('result3.html',y_pred=y_pred)
+
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
